ff567e8847b626f003dbd4ac184a30a28fb66b02
Labreo <74888611+Labreo@users.noreply.github.com>
2025-07-20 05:31:29 +0530

    create a contact component

--- App.jsx
+++ App.jsx
@@ -1,9 +1,12 @@
 import Header from "./components/Header"; 
 import Entry from "./components/Entry";
+import Contact from "./components/Contact";
 export default function App(){
  return(<>
- <Header />
- <Entry />
+ <Contact />
+ <Contact />
+ <Contact />
+  <Contact />
  </>
  );
 }
