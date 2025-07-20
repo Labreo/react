faa16a1fe9c2e2b6fc3bff24409b9484a6a0b920
Labreo <74888611+Labreo@users.noreply.github.com>
2025-07-20 05:46:44 +0530

    passing data into a componenet

--- App.jsx
+++ App.jsx
@@ -3,10 +3,10 @@ import Entry from "./components/Entry";
 import Contact from "./components/Contact";
 export default function App(){
  return(<>
- <Contact />
- <Contact />
- <Contact />
-  <Contact />
+ <Contact img="./images/mr-whiskerson.png" name="Mr. Whiskerson" number="(212) 555-1234" email="mr.whiskaz@catnap.meow" />
+ <Contact img="./images/fluffykins.png" name="Fluffykins" number="(212) 555-2345" email="fluff@me.com"  />
+ <Contact img="./images/felix.png" name="Felix" number="(212) 555-4567" email="thecat@hotmail.com" />
+  <Contact img="./images/pumpkin.png" name="Pumpkin" number="(0800) CAT KING" email="pumpkin@scrimba.com" />
  </>
  );
 }
