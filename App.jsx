5774ba1e2a3f9e5cd1c804ec691393d912eddd35
Labreo <74888611+Labreo@users.noreply.github.com>
2025-07-22 10:25:45 +0530

    key

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ import Contact from "./components/Contact";
 import Joke from "./components/Joke";
 import data from "./data"
 
-const dataElements = data.map((entry)=>{return <Entry img={entry.img} title={entry.title} country={entry.country} googleMapsLink={entry.googleMapsLink} dates={entry.dates} text={entry.text} /> }) 
+const dataElements = data.map((entry)=>{return <Entry img={entry.img} title={entry.title} country={entry.country} googleMapsLink={entry.googleMapsLink} dates={entry.dates} text={entry.text} key={entry.id} /> }) 
 
 export default function App(){
  return(<>
