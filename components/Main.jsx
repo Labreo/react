e286c4bf661ddf26a359e3c07a09b16ebbad69d1
Labreo <74888611+Labreo@users.noreply.github.com>
2025-07-23 02:31:53 +0530

    map ingredient list

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,14 +1,30 @@
 export default function Main() {
+    const ingredients=["Chicken","Oregano","Tomatoes"]
+    const x=ingredients.map(ingredient=><li key={ingredient}>{ingredient}</li>)
+    function formSubmit(event){
+        event.preventDefault()
+        const formData= new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        console.log(newIngredient)
+        ingredients.push(newIngredient)
+        console.log(ingredients)
+    }
+
     return (
+        
         <main>
-            <form className="add-ingredient-form">
+            <form className="add-ingredient-form" onSubmit={formSubmit}>
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {x}
+            </ul>
         </main>
     )
 }
