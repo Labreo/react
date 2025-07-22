08d3206d0e1018c999c910ca767b77115e478153
Labreo <74888611+Labreo@users.noreply.github.com>
2025-07-22 10:24:32 +0530

    map entry components

--- components/Entry.jsx
+++ components/Entry.jsx
@@ -1,28 +1,20 @@
-
 export default function Entry(props){
-    return(<>
+    return (
+        <div className="entry-container">
+            <img className="main-image" src={props.img.src} alt={props.img.alt} />
+            
+            <div className="Body">
+                <span className="FirstLine">
+                    <img className="Marker" src="images/marker.png" width="7px" height="9.55px" />
+                    <p className="PlaceName">{props.country}</p>
+                    <a href={props.googleMapsLink}>View on Google Maps</a>
+                </span>
 
-        <div className="Body">
-            <span className="FirstLine">
-            <img className="Marker" src="images/marker.png" width="7px" height="9.55px" />
-            <p className="PlaceName">{props.country}</p>
-            <a href={props.googleMapsLink}>View on Google Maps</a>
-       </span>
-       
-        
-        <span>
-            <h1>{props.title}</h1>
-        </span>
-        
-            <h4>{props.dates}</h4>
-        
-        <span>
-            <p>
-                {props.text}
-            </p>
-        </span>
+                <h1>{props.title}</h1>
+                <h4>{props.dates}</h4>
+
+                <p>{props.text}</p>
+            </div>
         </div>
-        <img className="main-image" src={props.img.src} alt={props.img.alt} />
-</>
-    )
+    );
 }
